de90eb188e07ab6c03304170c32bf8aa2a139483
Warnerhannah <warnerhannah1@yahoo.com>
2019-03-01 13:16:04 -0800

    fixed timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,45 @@
 //global variables
-var countdown = 120;
 var intervalId;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
+var questionCounter = 1;
+var countdown = 31;
 
 $("#countdown, #timeleft, #mainsection, #submitButton").hide();
 
 //when start is clicked begin quiz
 $("#startgame").on("click", function () {
-    $("#startgame").hide();
-    $("#countdown, #timeleft, #mainsection, #submitButton").show();
-    intervalId = setInterval(countDown, 1000);
+    startGame();
+    questionCounter =1;
 });
+
+function startGame() {
+    setTime();
+    $("#startgame").hide();
+    $("#correctanswers, #incorrectanswers").hide();
+    $("#countdown, #timeleft, #mainsection, #submitButton").show();
+
+    if (questionCounter===1) {
+        $("#q2, #q3, #q4, #q5").hide();
+    }
+    else if (questionCounter===2) {
+        $("#q2").show();
+        $("#q1, #q3, #q4, #q5").hide();
+    }
+    else if (questionCounter===3) {
+        $("#q3").show();
+        $("#q2, #q1, #q4, #q5").hide();
+    }
+    else if (questionCounter===4) {
+        $("#q4").show();
+        $("#q2, #q3, #q1, #q5").hide();
+    }
+    else if (questionCounter===5) {
+        $("#q5").show();
+        $("#q2, #q3, #q4, #q1").hide();
+        $("#submitbutton").html("Submit");
+    }
+}
 
 //count down function and interval function
 function countDown() {
@@ -20,35 +48,61 @@ function countDown() {
     if (countdown === 0) {
         alert("Time is up!");
         clearInterval(intervalId);
-        scoreGame();
+        giveAnswer();
+        startGame();
     }
 }
 
-//if submit is clicked
+function setTime() {
+    clearInterval(intervalId);
+    intervalId = setInterval(countDown, 1000);
+}
+
+//if submit or next is clicked
 $("#submitButton").on("click", function () {
-    scoreGame()
+    clearInterval(intervalId);
+    if (questionCounter===5) {
+        giveAnswer();
+        scoreGame();
+    }
+    else {
+        giveAnswer();
+        startGame();
+    }
 });
 
-//tally and display score
-function scoreGame() {
-    clearInterval(intervalId);
-
-    //tally scores
-    for (i = 0; i < 5; i++) {
-        var userChoice = $('input[name=answer' + i + ']:checked').val();
+function giveAnswer() {
+    var userChoice = $('input[name=answer' + questionCounter + ']:checked').val();
         if (userChoice === "correct") {
+            alert("Correct Answer!");
             correctAnswers++;
+            questionCounter++;
+            startGame();
         }
         else {
+            alert("Incorrect Answer!");
             incorrectAnswers++;
+            questionCounter++;
+            startGame();
         }
-    }
+
+}
+
+//tally and display final score
+function scoreGame() {
+    clearInterval(intervalId);
     alert("Thanks for playing!")
 
     //print scores
+    $("#correctanswers, #incorrectanswers").show();
     $("#correctanswers").html("You got " + correctAnswers + " answers right.")
     $("#incorrectanswers").html("And " + incorrectAnswers + " answers wrong.")
 
     //clear questions and stuff off page
     $("#countdown, #timeleft, #mainsection, button").hide();
+    $("#startgame").show().html("Restart");
+
+    for (i = 0; i < 5; i++) {
+        $('input[name=answer' + i + ']:checked').attr('checked',false);
+    }
 };
